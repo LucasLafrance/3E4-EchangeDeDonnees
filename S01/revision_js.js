800ef411fabec4f85fc339f3fc3d47693392e112
Lucas Lafrance <1829354@cstj.qc.ca>
2021-08-26 16:58:21 -0400

    Fin du cours 02

--- S01/revision_js.js
+++ S01/revision_js.js
@@ -15,20 +15,78 @@ const test4 = true * false;
 console.log(test4);
 const test5 = 125 + "9";
 console.log(test5);
-console.log(("b"+"a"+ +"a"+"a"));
+console.log(("b" + "a" + +"a" + "a"));
 
-function displayUser(prenom, age ){
+function displayUser(prenom, age) {
 
-     
+
     console.log(`\n Bonjour je m'appelle ${prenom} et j'ai ${age} ans`);
-        
+
 }
 displayUser("Lucas", 20);
 
 const fruits = ["Kiwi", "Fraise", "Banane", "Framboise", "Mangue"];
 
-for(let fruit of fruits){
+for (let fruit of fruits) {
 
     console.log(fruit);
 }
-    
+
+fruits.push("Pomme");
+console.log("=====================");
+
+fruits.forEach(fruit => console.log(fruit));
+
+const sum = (a, b) => a + b;
+
+// La sum est fait avec c'est 2 nombres
+const result = sum(12, 8);
+console.log(result);
+
+//Va filtrer ce qui est vrai ou non selon la condition
+const filtre = fruits.filter(f => f.length > 5);
+console.log(filtre);
+
+
+const numbers = [10, 20, 30, 40];
+const MULTIPLIER = 3;
+
+// map va multiplier les nombres par MULTIPLIER(3) et va nous sortir ce qui est vrai à la condition de filter
+const products = numbers.map(n => n * MULTIPLIER).filter(n => n > 75);
+
+console.log(products);
+console.log(numbers);
+
+//Création d'objets
+const spiderman = {
+    hero: "Spiderman",
+    alterEgo: "Peter Parker",
+    movies:[{title:"Spiderman 1"},{title:"Spiderman 2"},{title:"Spiderman 3"}]
+}
+
+const ironman = {
+    hero: "Ironman",
+    alterEgo: "Tony Stark",
+    movies:[{title:"Ironman 1"},{title:"Ironmann 2"},{title:"Ironman 3"}]
+}
+
+//Création de la class Avenger
+class Avenger{
+
+    constructor(hero, alterEgo, movies){
+        this.hero = hero;
+        this.alterEgo = alterEgo;
+        this.movies =movies;
+
+
+
+    }
+
+        test(){
+
+        }
+}
+
+const oneAvenger = new Avenger("Hulk", "Bruce Baner", [{title:"Hulk 1"},{title:"Hulk 2"},{title:"Hulk 3"}])
+console.log(oneAvenger.hero);
+console.log(spiderman.hero);
