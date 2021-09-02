416713cfd540ebec72dfc6a80517cb61c16eb343
Lucas Lafrance <1829354@cstj.qc.ca>
2021-09-02 16:43:39 -0400

    find de cours 4 (2sept)

--- S02/src/app.js
+++ S02/src/app.js
@@ -21,17 +21,50 @@ app.get("/date", (req, res) =>{
 
 });
 
+// maths/somme 
+// maths/difference
+// maths/produit
+// maths/quotient
+// maths/reste
 
-app.get("/somme", (req, res)=>{
+app.get("/maths/:operation", (req, res)=>{
+
+    const operation = req.params.operation;
+    console.log(operation);
+
+    const a =  parseInt(req.query.a, 10) ;
+    const b = parseInt(req.query.b, 10);
+    let result = 0;
+switch(operation) {
+
+    case 'somme':
+        result = a+b;
+        break;
+    case 'difference':
+        result = a-b;
+        break; 
+    case 'produit':
+        result = a*b;
+        break;
+    case 'quotient':
+        result = a/b;
+        break;
+    case 'reste':
+        result = a%b;
+        break;
+    default:
+   // return res.status(400).end();
+    return res.send("Opération non reconnue!").end();
+
+}
 
 //console.log(req.query);
 
-const a =  parseInt(req.query.a, 10) ;
-const b = parseInt(req.query.b, 10);
+
 
 res.status(200);// il dit que ça fonctionne
 res.set("Content-Type", "text/html"); // Il dit qu'il va lui envoie du texte plain. "normal"
-res.send(`<strong>${a+b}`); // est la même chose que la ligne 22
+res.send(`<strong>${result}`); // est la même chose que la ligne 22
 //res.send((a+b).toString());
 
 
